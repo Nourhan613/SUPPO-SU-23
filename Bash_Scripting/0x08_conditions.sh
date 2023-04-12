06ed66b1543a34bcd84f794f7fb81ce956e5b435
Nourhan Mohamed <98773024+Nourhan613@users.noreply.github.com>
2023-04-12 23:58:48 +0200

    Update and rename 0x08_conditions to 0x08_conditions.sh

---
+++ Bash_Scripting/0x08_conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# prints "Requirement met" only when the number of dirs in the variable $PATH equals 2, otherwise print number of dirs.
+PATH = find . -type d | wc -l
+if [[$PATH -eq 2]]
+then
+echo “Requirement met”
+else
+echo $PATH
+fi
