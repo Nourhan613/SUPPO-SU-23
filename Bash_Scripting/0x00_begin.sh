deb2151b3420d76a6878bf474621aceca5d24f78
Nourhan Mohamed <98773024+Nourhan613@users.noreply.github.com>
2023-04-12 23:07:18 +0200

    Update 0x00_begin.sh

--- Bash_Scripting/0x00_begin.sh
+++ Bash_Scripting/0x00_begin.sh
@@ -1,7 +1,3 @@
-Question 0
-Write a  script that creates a folder called "Bash Scripting "  and create all required files for this task (all at once) in this folder .
-Lastly, print the folders content.
-
 #!/bin/bash
 
 # creates a folder called "Bash Scripting"
